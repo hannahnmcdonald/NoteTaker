47b4a05484a36c5d02c4f055cfa94e903d4eeaa7
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-09-02 17:46:07 -0400

    trying to fix heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 //  Requirements
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-const fs = require('fs');
-const path = require('path');
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+// const fs = require('fs');
+// const path = require('path');
 
 // Initialize App + Create Port
 const app = express();
@@ -11,10 +11,10 @@ const PORT = process.env.PORT || 3001;
 
 // Set Up Middleware
 app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true}));
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
 // Listen for connections
 app.listen(PORT, () =>
