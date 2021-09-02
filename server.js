b621d75247b00f8df0e2db1ad0318f15145ca59a
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-09-02 15:22:02 -0400

    middleware added to server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 //  Requirements
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // Initialize App + Create Port
 const app = express();
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001;
 
 // Set Up Middleware
 app.use(express.static('public'));
-
-
+asserts.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // Listen for connections
 app.listen(PORT, () =>
