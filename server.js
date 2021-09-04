fb661810232ace1b3f32da80df6745afbbbd6818
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-09-04 12:18:11 -0400

    routine

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 // Initialize App + Create Port
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Set Up Middleware
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-// app.use(express.json());
+// app.use('/api', apiRoutes);
+app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
-app.use('/', htmlRoutes);
+// app.use('/', htmlRoutes);
 
 // Listen for connections
 app.listen(PORT, () =>
