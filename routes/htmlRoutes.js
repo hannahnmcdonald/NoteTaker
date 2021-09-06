fb1225cb90a0093a7b1781fa8d280c0a500de6e8
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-09-06 17:57:53 -0400

    troubleshooting

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,7 +8,7 @@ router.get('notes', (req, res) => {
 });
 
 // Get route for home page
-router.get('/', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../pub;ic/index.html'));
 });
 
--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 // Initialize App + Create Port
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 // Set Up Middleware
 app.use(express.static('public'));
 // app.use('/api', apiRoutes);
-app.use(express.json());
+// app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // app.use('/', htmlRoutes);
 
